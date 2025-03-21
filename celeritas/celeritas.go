d2f227292045205000fed23f2773052b406ce8a3
Fouche du Preez <fouche@limehouse.co.za>
2025-03-21 14:48:26 +0200

    added jet template support

--- celeritas/celeritas.go
+++ celeritas/celeritas.go
@@ -2,6 +2,7 @@ package celeritas
 
 import (
 	"fmt"
+	"github.com/CloudyKit/jet/v6"
 	"github.com/fouched/celeritas/render"
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
@@ -23,6 +24,7 @@ type Celeritas struct {
 	RootPath string
 	Routes   *chi.Mux
 	Render   *render.Render
+	JetViews *jet.Set
 	config   config // no reason to export this
 }
 
@@ -66,6 +68,12 @@ func (c *Celeritas) New(rootPath string) error {
 		port:     os.Getenv("PORT"),
 		renderer: os.Getenv("RENDERER"),
 	}
+
+	var views = jet.NewSet(
+		jet.NewOSFileSystemLoader(fmt.Sprintf("%s/views", rootPath)),
+		jet.InDevelopmentMode(),
+	)
+	c.JetViews = views
 
 	c.createRenderer()
 
@@ -124,6 +132,7 @@ func (c *Celeritas) createRenderer() {
 		Renderer: c.config.renderer,
 		RootPath: c.RootPath,
 		Port:     c.config.port,
+		JetViews: c.JetViews,
 	}
 	c.Render = &myRenderer
 }
